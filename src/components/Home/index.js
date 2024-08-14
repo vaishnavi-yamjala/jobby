d52e5c0b34711a77780af849c2e20aa2d0d28813
vaishnavi-yamjala <vaishnaviyamjala@gmail.com>
2024-08-14 15:12:01 +0000

    update home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,7 +14,7 @@ const Home = () => {
       <Header />
       <div className="home-page">
         <div className="home-page-content">
-          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <h1 className="home-heading">Find The Job That Fits Your path of Life</h1>
           <p className="home-description">
             Millions of people are searching for jobs, salary
             information,company reviews. Find the job that fits your abilities
